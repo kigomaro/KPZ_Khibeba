cb63042780cb861950bde65567a658f4940bbcee
Vadym Khibeba <136254836+kigomaro@users.noreply.github.com>
2024-04-12 12:46:14 +0300

    LR1.py

--- LR1.py
+++ LR1.py
@@ -1,21 +1,14 @@
 import pandas as pd
-import os
-from datetime import datetime
-csv_file_path = 'filename.csv'
-if os.path.exists(csv_file_path):
-    dataframe = pd.read_csv(csv_file_path, index_col=0)
-else:
-    columns = ['year', 'month', 'day', 'hour', 'minute', 'second']
-    dataframe = pd.DataFrame(columns=columns)
-now = datetime.now()
-new_row = pd.DataFrame([{
-    'year': now.year,
-    'month': now.month,
-    'day': now.day,
-    'hour': now.hour,
-    'minute': now.minute,
-    'second': now.second
-}])
-dataframe = pd.concat([dataframe, new_row], ignore_index=True)
-dataframe.to_csv(csv_file_path, index=False)
-print('CSV file path:', csv_file_path)
+import datetime as dt
+
+try:
+    df = pd.read_csv('filename.csv')
+except FileNotFoundError:
+    df = pd.DataFrame(columns = ['year', 'month', 'day', 'hour', 'minute', 'second'])
+
+now = dt.datetime.now()
+
+df.loc[len(df)] = [now.year, now.month, now.day, now.hour, now.minute, now.second]
+df.to_csv("filename.csv", index=False)
+
+print(df)
